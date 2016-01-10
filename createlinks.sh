dcd371ec20d33bb18cf62de22c61efbeda88622b
Rick Davidson <rd_187@yahoo.com>
2016-01-10 18:58:47 -0500

    modified .tmux.conf and created script for symlinks

---
+++ createlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#This script can be used to create the
+#symbolic links. Comment out what you don't
+#want.
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf  
+ln -s ~/.dotfiles/.vimrc ~/.vimrc 
