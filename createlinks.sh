118bb91aa066531b5189b7aa5bce9654a0acb172
Rick Davidson <rd_187@yahoo.com>
2016-10-22 12:37:50 -0400

    delete createlinks.sh

--- createlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#This script can be used to create the
-#symbolic links. Comment out what you don't
-#want.
-#Run this as the user you want to create
-#the links for.
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf  
-ln -s ~/.dotfiles/.vimrc ~/.vimrc 
